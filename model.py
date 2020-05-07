26a3f1a83130e5d564b5239fd062d4f05258c3ff
matt <mmatero@cs.stonybrook.edu>
2020-05-07 12:41:35 -0400

    Commented my_lstm_regressor

--- model.py
+++ model.py
@@ -41,19 +41,22 @@ class my_lstm_regressor(nn.Module):
 
     def __init__(self, input_size, hidden_size):
         super(my_lstm_regressor, self).__init__()
+    
+        self.hs = hidden_size # internal hidden size
+        self.inp = input_size # input feature space
 
-        self.hs = hidden_size
-        self.inp = input_size
-
+        # define our LSTM model and linear decoder
         self.lstm_cell = nn.LSTMCell(self.inp, self.hs)
         self.decoder = nn.Linear(self.hs, 1)
 
+        # tracks if hidden states should be moved to GPU on init
         self.run_cuda = torch.cuda.is_available()
 
     def forward(self, inputs, hidden):
         seq_len = inputs.shape[0] # (len, batch, features)
         hidden_state, cell_state = hidden
         
+        # loop through our LSTM cell and gather our states
         hidden_states = []
         for i in range(seq_len):
             hidden_state, cell_state = self.lstm_cell(inputs[i], (hidden_state, cell_state))
@@ -63,8 +66,10 @@ class my_lstm_regressor(nn.Module):
 
     def init_hidden(self, batch_size):
         weight = next(self.parameters())
+        # init hidden state and cell state to tensor of 0s, both of size self.hidden_size
         hidden, cell = (weight.new_zeros(batch_size, self.hs), weight.new_zeros(batch_size, self.hs))
 
+        # confirm they are on GPU
         if self.run_cuda:
             hidden.cuda()
             cell.cuda()
